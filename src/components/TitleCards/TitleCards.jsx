f88700d293da9b80427120dd1ed6921f0a7d7c65
Kevin Uduji <kevinuduji@gmail.com>
2024-08-03 21:37:34 -0500

    Created Home Page
    Includes:
    • Movie Cards
    • Navbar (No links yet)
    • Hero Section (Not working yet)
    • All Images and movies parsed
       through in a JS file
    • Footer

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from 'react'
+import './TitleCards.css'
+import card_data from '../../assets/cards/Cards_data.js'
+
+
+
+const TitleCards = ({title, category}) => {
+
+  const cardsRef = useRef();
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
+
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, [])
+
+  return (
+    <div className='title-cards'>
+        <h2>{title?title:"Popular on Netfilms"}</h2>
+        <div className="card-list" ref={cardsRef}>
+            {
+                card_data.map((card, index) => {
+                    return (
+                        <div className="card" key={index}>
+                            <img src={card.image} alt="" />
+                            <p>{card.name}</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    </div>
+  )
+}
+
+export default TitleCards
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import TitleCards from '../../components/TitleCards/TitleCards'
+import Footer from '../../components/Footer/Footer'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className='banner-img'/>
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className='caption-img'/>
+          <p>Former elite operative Jack Harper is back in action to stop a covert organization from unleashing global chaos. Racing against time, he must use his combat skills and wits to thwart an apocalyptic plot. Buckle up for a relentless, high-stakes thrill ride!</p>
+          <div className="hero-btns">
+            <button className='btn'><img src={play_icon} alt="" />Play</button>
+            <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
+          </div>
+          <TitleCards />
+        </div>  
+      </div>
+      <div className="more-cards">
+        <TitleCards title={"Most Popular Movies"} />
+        <TitleCards title={"Only on Netfilms"}/>
+        <TitleCards title={"Upcoming Series"}/>
+        <TitleCards title={"Your Top Picks"}/>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
